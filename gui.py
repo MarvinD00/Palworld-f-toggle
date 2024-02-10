8c908e5d2013363544edd488e70432ce31d96943
GleichFetzung <egotricksegotricks@gmail.com>
2024-02-10 18:44:14 +0100

    Improve layout

--- gui.py
+++ gui.py
@@ -7,35 +7,44 @@ class ToggleApp:
 	def __init__(self):
 		self.root = Tk()
 		self.root.title("Toggle GUI")
-		self.root.geometry("300x400")
+		self.root.geometry("300x200")
 
-		self.running_label = Label(self.root, text="Not Running", fg="red")
-		self.running_label.pack(pady=10)
+		self.label_frame = Frame(self.root)
+		self.label_frame.pack()
+
+		self.entry_frame = Frame(self.root)
+		self.entry_frame.pack()
+
+		self.button_frame = Frame(self.root)
+		self.button_frame.pack()
 
 		self.toggle = tg.toggle()
 
-		self.select_start_key_label = Label(self.root, text="Select start Key")
-		self.select_start_key_label.pack(pady=10)
 
-		self.select_stop_key_label = Label(self.root, text="Select stop Key")
-		self.select_stop_key_label.pack(pady=10)
+		self.select_start_key_label = Label(self.label_frame, text="Select start Key")
+		self.select_start_key_label.pack(padx=10, pady=10, side=LEFT)
 
-		self.start_key_entry = Entry(self.root, width=20, state="readonly")
-		self.start_key_entry.pack(pady=10)
+		self.select_stop_key_label = Label(self.label_frame, text="Select stop Key")
+		self.select_stop_key_label.pack(padx=10, pady=10, side=LEFT)
 
-		self.stop_key_entry = Entry(self.root, width=20, state="readonly")
-		self.stop_key_entry.pack(pady=10)
+		self.start_key_entry = Entry(self.entry_frame, width=10, state="readonly")
+		self.start_key_entry.pack(padx=15, pady=10, side=LEFT)
+
+		self.stop_key_entry = Entry(self.entry_frame, width=10, state="readonly")
+		self.stop_key_entry.pack(padx=15, pady=10, side=LEFT)
 
 		self.start_key_entry.bind("<FocusIn>", self.on_start_focus_in)
 		self.stop_key_entry.bind("<FocusIn>", self.on_stop_focus_in)
 
-		self.toggle_button = Button(self.root, text="Start Toggle", command=self.start_toggle)
-		self.toggle_button.pack(pady=10)
+		self.toggle_button = Button(self.button_frame, text="Start Toggle", command=self.start_toggle)
+		self.toggle_button.pack(padx=10, pady=10, side=LEFT)
 
-		self.stop_button = Button(self.root, text="Stop Toggle", command=self.stop_toggle)
-		self.stop_button.pack(pady=10)
-		self.stop_button.config(state=DISABLED)
+		self.stop_button = Button(self.button_frame, text="Stop Toggle", command=self.stop_toggle, state=DISABLED)
+		self.stop_button.pack(padx=10, pady=10, side=LEFT)
 
+		self.running_label = Label(self.root, text="Not Running", fg="red")
+		self.running_label.pack(pady=10)
+		
 		self.listener = None
 
 		self.root.mainloop()
