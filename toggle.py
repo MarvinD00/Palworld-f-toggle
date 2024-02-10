3180f1dc2a56a51587b76a14bbc699305ac16b1a
MarvinD00 <marvin.diedrich@gmx.de>
2024-02-10 18:11:47 +0100

    toggle: improve functionality of toggle and threading
    we improve the functionality of the toggle and threading.
    We can now receive input events during simulated key presses for f key.
    Also merged the 2 set_key functions into 1 since they did the same anyway.

--- toggle.py
+++ toggle.py
@@ -8,26 +8,34 @@ class toggle:
 		self.stop_key = None
 		self.toggle = False
 		self.thread = None
+		self.listener = None
+		self.hold = False
 
 	def start_toggle(self):
 		if not self.toggle:
 			self.swap()
+			self.listener = keyboard.Listener(
+				on_press=self.on_press)
+			self.listener.start()
 
-	def set_start_key(self):
+	def set_key(self, start):
 		with keyboard.Events() as events:
 			event = events.get(10.0)
 			if event is None:
-				return('You did not press a key within ten seconds, try again')
+				return 'You did not press a key within ten seconds, try again'
 			else:
-				return('{}'.format(event))
-			
-	def set_stop_key(self):
-		with keyboard.Events() as events:
-			event = events.get(10.0)
-			if event is None:
-				return('You did not press a key within ten seconds, try again')
-			else:
-				return('{}'.format(event))
+				if hasattr(event.key,'char'):
+					if start:
+						self.start_key = event.key.char
+					else:
+						self.stop_key = event.key.char
+					return event.key.char
+				else:
+					if start:
+						self.start_key = event.key
+					else:
+						self.stop_key = event.key
+					return event.key
 
 	def stop_toggle(self):
 		if self.toggle:
@@ -38,27 +46,33 @@ class toggle:
 
 	def on_press(self, key):
 		try:
-			if key.char == 'f':
+			if key.char == self.start_key:
 				self.start_thread()
-			elif key.char == 'c':
+			elif key.char == self.stop_key:
 				self.stop_thread()
 		except AttributeError:
 			pass
 
 	def start_thread(self):
-		if self.thread is None and not self.thread.is_alive():
-			self.thread = threading.Thread(target=self.hold_f)
-			self.thread.start()
+		if self.thread is None or not self.thread.is_alive():
+			self.hold = True
+			print('Starting Thread')
+			with keyboard.Listener(on_press=self.on_press) as listener:
+				self.thread = threading.Thread(target=self.hold_f)
+				self.thread.start()
 
 	def stop_thread(self):
 		if self.thread and self.thread.is_alive():
+			self.hold = False
 			self.thread.join()
+			self.thread.stop()
 			self.thread = None
 
 	def hold_f(self):
-		while self.toggle:
-			pyautogui.press('f')
-			time.sleep(0.1)
+		while self.hold:
+			print(threading.active_count())
+			keyboard.Controller().press('f')
+			time.sleep(0.01)
 
 	def swap(self):
 		self.toggle = not self.toggle
