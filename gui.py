1d55951ec300c2323de6bd19a41bc140450a6ed4
GleichFetzung <egotricksegotricks@gmail.com>
2024-02-10 16:28:12 +0100

    Palworld f-toggle: Improved gui
    - added running labels
    - resized gui

--- gui.py
+++ gui.py
@@ -1,35 +1,47 @@
-import tkinter as tk
-from pynput import keyboard
-import threading
-import pyautogui
-import time
+from tkinter import *
+from tkinter import ttk
 import toggle as tg
 
 class ToggleApp:
 
-	def __init__(self, master):
-		self.master = master
-		self.master.title("Toggle GUI")
+	def __init__(self):
+		self.root = Tk()
+		self.root.title("Toggle GUI")
+		self.root.geometry("300x300")
 
-		self.toggle_button = tk.Button(master, text="Start Toggle", command=self.start_toggle)
+		self.running_label = Label(self.root, text="Not Running", fg="red")
+		self.running_label.pack(pady=10)
+
+		self.keys = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+		self.select_key_label = Label(self.root, text="Select Key")
+		self.select_key_label.pack(pady=10)
+
+		self.cmb = ttk.Combobox(self.root, values=self.keys)		
+		self.cmb.pack(pady=10)
+
+		self.toggle_button = Button(self.root, text="Start Toggle", command=self.start_toggle)
 		self.toggle_button.pack(pady=10)
 
-		self.stop_button = tk.Button(master, text="Stop Toggle", command=self.stop_toggle)
+		self.stop_button = Button(self.root, text="Stop Toggle", command=self.stop_toggle)
 		self.stop_button.pack(pady=10)
-		self.stop_button.config(state=tk.DISABLED)
+		self.stop_button.config(state=DISABLED)
 
 		self.toggle = tg.toggle()
 		self.listener = None
 
+		self.root.mainloop()
+
 	def start_toggle(self):
-		self.toggle_button.config(state=tk.DISABLED)
-		self.stop_button.config(state=tk.NORMAL)
+		self.running_label.config(text="Running", fg="green")
+		self.toggle_button.config(state=DISABLED)
+		self.stop_button.config(state=NORMAL)
 		self.toggle.start_toggle()
-		self.listener = keyboard.Listener(on_press=self.on_press)
 
 	def stop_toggle(self):
-		self.toggle_button.config(state=tk.NORMAL)
-		self.stop_button.config(state=tk.DISABLED)
+		self.running_label.config(text="Not Running", fg="red")
+		self.toggle_button.config(state=NORMAL)
+		self.stop_button.config(state=DISABLED)
 		self.toggle.stop_toggle()
 		if self.listener:
 			self.listener.stop()
@@ -40,6 +52,4 @@ class ToggleApp:
 		self.toggle.on_press(key)
 
 if __name__ == "__main__":
-	root = tk.Tk()
-	app = ToggleApp(root)
-	root.mainloop()
+	ToggleApp()
